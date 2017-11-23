7eba82b582d1fcec6d136fa46420c3a264007ed2
zenga2 <zenga2@foxmail.com>
2017-11-23 18:24:07 +0800

    Add polyfill

--- src/common/polyfill/array.js
+++ src/common/polyfill/array.js
@@ -1,13 +1,39 @@
 if (!('includes' in Array.prototype)) {
   Object.defineProperty(Array.prototype, 'includes', {
-    value (item) {
+    value(item) {
       return this.indexOf(item) > -1
     }
   })
 }
 
+if (!('findIndex' in Array.prototype)) {
+  Object.defineProperty(Array.prototype, 'findIndex', {
+    value(fn) {
+      for (let i = 0, len = this.length; i < len; i++) {
+        let item = this[i]
+        if (!!fn(item, i, this) === true) {
+          return i
+        }
+      }
+    }
+  })
+}
+
+if (!('find' in Array.prototype)) {
+  Object.defineProperty(Array.prototype, 'find', {
+    value(fn) {
+      for (let i = 0, len = this.length; i < len; i++) {
+        let item = this[i]
+        if (!!fn(item, i, this) === true) {
+          return item
+        }
+      }
+    }
+  })
+}
+
 Object.defineProperty(Array.prototype, 'remove', {
-  value (item) {
+  value(item) {
     let index = this.indexOf(item)
 
     if (index > -1) {
