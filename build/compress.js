db4b144525cf2286b54edaba9c26c97994f96ffc
zenga2 <zenga2@foxmail.com>
2017-12-21 19:52:31 +0800

    Optimize types support

--- build/compress.js
+++ build/compress.js
@@ -3,16 +3,21 @@ var path = require('path')
 var archiver = require('archiver')
 var config = require('../package')
 
-var zipFileName = config.buildName + '-' + config.version + '.zip'
-var output = fs.createWriteStream(path.resolve(__dirname, '../' + zipFileName))
+var obj = JSON.parse(process.env.npm_config_argv)
+var zipFileName = obj.remain[0]
+zipFileName = zipFileName
+  ? zipFileName + '.zip'
+  : config.buildName + '-' + config.version + '.zip'
+
+var output = fs.createWriteStream(path.resolve(__dirname, '../dist_zip/' + zipFileName))
 
 var archive = archiver('zip', {
-    zlib: {level: 9}
+  zlib: {level: 9}
 })
 
 output.on('close', function () {
-    console.log(archive.pointer() + ' total bytes')
-    console.log('Zip complete')
+  console.log(archive.pointer() + ' total bytes')
+  console.log('Zip complete')
 })
 
 archive.pipe(output)
