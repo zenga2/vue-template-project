2b8d61196e6030c6a0b15b547f754128a1e72466
zenga2 <zenga2@foxmail.com>
2018-02-07 15:02:22 +0800

    Optimize curry Function

--- src/common/utils/help.js
+++ src/common/utils/help.js
@@ -4,21 +4,21 @@ function curry(fn) {
 
   return function () {
     let thisArg = this
-    let args = Array.from(arguments)
+    let args = []
 
-    if (args.length >= len) {
-      return fn.apply(thisArg, args)
-    }
+    let _curry = function () {
+      if (arguments.length > 0) {
+        Array.from(arguments).forEach(item => args.push(item))
 
-    return function _curry() {
-      Array.from(arguments).forEach(item => args.push(item))
-
-      if (args.length >= len) {
-        return fn.apply(thisArg, args)
+        if (args.length >= len) {
+          return fn.apply(thisArg, args)
+        }
       }
 
       return _curry
     }
+
+    return _curry.apply(null, arguments)
   }
 }
 
