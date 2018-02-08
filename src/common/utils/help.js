b2f1f4abb6686bec04b9aa902bd8eea1e0a234c0
zenga2 <zenga2@foxmail.com>
2018-02-08 16:36:16 +0800

    Optimize curry Function

--- src/common/utils/help.js
+++ src/common/utils/help.js
@@ -1,12 +1,12 @@
 // 柯里化
 function curry(fn, thisArg) {
   let len = fn.length
-  
+
   let _curry = function () {
     let args = this.slice()
 
     if (arguments.length > 0) {
-      Array.from(arguments).forEach(item => args.push(item))
+      args = args.concat(Array.from(arguments))
 
       if (args.length >= len) {
         return fn.apply(thisArg, args)
@@ -17,6 +17,25 @@ function curry(fn, thisArg) {
   }
 
   return _curry.bind([])
+}
+
+function curry_two(fn, thisArg) {
+  let len = fn.length
+
+  let _curry = function () {
+    let args = Array.from(arguments)
+
+    if (args.length >= len) {
+      return fn.apply(thisArg, args)
+    }
+
+    return function () {
+      let arr = args.concat(Array.from(arguments))
+      return _curry.apply(null, arr)
+    }
+  }
+
+  return _curry
 }
 
 export {curry}
