9ae591863b3232f9c2d7aae4a8e7191efd0df217
zenga2 <zenga2@foxmail.com>
2018-02-08 16:38:03 +0800

    Optimize ldUtils

--- src/common/utils/ldUtils.js
+++ src/common/utils/ldUtils.js
@@ -110,7 +110,7 @@ function modifyTitle(text) {
 }
 
 function isMobileNumber(mobileNumber) {
-  let pattern = /^((\d{11})|^((\d{7,8})|(\d{4}|\d{3})-(\d{7,8})|(\d{4}|\d{3})-(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1})|(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1}))$)$/
+  let pattern = /^((\d{11})|^((\d{7,8})|(\d{4}|\d{3})-(\d{7,8})|(\d{4}|\d{3})-(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d)|(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d))$)$/
 
   return pattern.test(mobileNumber)
 }
