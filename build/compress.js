fca03de472d5992297487be91ba56df68ad3aafe
zenga2 <zenga2@foxmail.com>
2018-03-26 18:14:13 +0800

    Optimize code

--- build/compress.js
+++ build/compress.js
@@ -1,8 +1,32 @@
 var fs = require('fs')
 var path = require('path')
 var archiver = require('archiver')
-var config = require('../package')
+var pkg = require('../package')
 
+var urlOfTargetFile = path.resolve(__dirname, '../src/global.config_1.js')
+var codeStr = fs.readFileSync(
+  path.resolve(__dirname, '../src/global.config.js'),
+  'utf8'
+).replace('export default', 'module.exports =')
+fs.writeFileSync(
+  urlOfTargetFile,
+  codeStr,
+  'utf8'
+)
+var config = require('../src/global.config_1')
+fs.unlinkSync(urlOfTargetFile)
+
+// update app version
+pkg.version = config.version
+fs.writeFileSync(
+  path.resolve(__dirname, '../package.json'),
+  JSON.stringify(pkg, null, '  ') + '\n',
+  'utf8',
+)
+
+if (!fs.existsSync('dist_zip')) {
+  fs.mkdirSync('dist_zip')
+}
 var obj = JSON.parse(process.env.npm_config_argv)
 var zipFileName = obj.remain[0]
 zipFileName = zipFileName
